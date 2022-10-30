ffcf2cb68fa95c8f096cfd9e184b1a4737a038a5
Nick <agnight4design@gmail.com>
2022-10-30 22:29:16 +0300

    fixed assets

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -4,6 +4,6 @@ import SamplePageImg from "@/components/SamplePageImg/SamplePageImg.vue";
 
 <template>
   <SamplePageImg
-    :img-src="'https://sun9-west.userapi.com/sun9-49/s/v1/ig2/B-uF9UafiRFp1X4AHDpXlxXggzSZ8PPyedjYOEjA-uURP65WlezJRI_ixnDHHwWPLB4fSO97YHSKxoCRmWzVhDUq.jpg?size=960x1280&quality=96&type=album'"
+    :img-src="'https://sun9-north.userapi.com/sun9-82/s/v1/ig2/437Y6qrqzeVdsc_V_uva7BzOvRuuEkmItmkfcnUfb_SDbvJBWYQeSHJsWkVcou_LqJnmG2-KqKGD2osrokfOrwlr.jpg?size=1280x960&quality=96&type=album'"
   />
 </template>
