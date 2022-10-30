8e7e8ae8abe4128605e4c323163d7212687677f1
Nick <agnight4design@gmail.com>
2022-10-30 22:27:02 +0300

    work with assets

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -3,5 +3,7 @@ import SamplePageImg from "@/components/SamplePageImg/SamplePageImg.vue";
 </script>
 
 <template>
-  <SamplePageImg :img-src="'../assets/seagulls.jpg'" />
+  <SamplePageImg
+    :img-src="'https://sun9-west.userapi.com/sun9-49/s/v1/ig2/B-uF9UafiRFp1X4AHDpXlxXggzSZ8PPyedjYOEjA-uURP65WlezJRI_ixnDHHwWPLB4fSO97YHSKxoCRmWzVhDUq.jpg?size=960x1280&quality=96&type=album'"
+  />
 </template>
